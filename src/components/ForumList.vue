42d6a522382cc3d4e166683815578e966fa0973b
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-10-01 16:47:52 +0200

    introduce category feature

--- src/components/ForumList.vue
+++ src/components/ForumList.vue
@@ -3,7 +3,11 @@
     <div class="forum-list">
 
       <h2 class="list-title">
-        <a href="#">Forums</a>
+        <router-link v-if="categoryId" :to="{name: 'Category', params: {id: categoryId}}">
+          {{ title }}
+        </router-link>
+
+        <span v-else>{{ title }}</span>
       </h2>
 
       <div class="forum-listing" v-for="forum in forums" :key="forum.id">
@@ -37,6 +41,14 @@ export default {
     forums: {
       required: true,
       type: Array
+    },
+    title: {
+      type: String,
+      default: 'Forums'
+    },
+    categoryId: {
+      required: false,
+      type: String
     }
   },
   methods: {
