1894c4f70c6347c223dc571a53a59a88f87b293f
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-10-02 17:44:27 +0200

    preparing the interface for post updates

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -4,43 +4,52 @@
          v-for="post in posts"
          :key="post.id"
     >
-
-       <div v-if="userById(post.userId)" class="user-info">
+      <div v-if="userById(post.userId)" class="user-info">
         <a href="#" class="user-name">{{userById(post.userId).name}}</a>
-
         <a href="#">
           <img class="avatar-large" :src="userById(post.userId).avatar" alt="">
         </a>
-
         <p class="desktop-only text-small">{{userById(post.userId).postsCount}} posts</p>
         <p class="desktop-only text-small">{{userById(post.userId).threadsCount}} threads</p>
-
       </div>
 
       <div class="post-content">
-        <div>
-          <p>
+        <div class="col-full">
+          <PostEditor v-if="editing === post.id" :post="post" />
+          <p v-else>
             {{post.text}}
           </p>
         </div>
+        <a
+          @click.prevent="toggleEditMode(post.id)"
+          href="#"
+          style="margin-left: auto; padding-left:10px;"
+          class="link-unstyled"
+          title="Make a change"
+        >
+          edit/post
+        </a>
       </div>
-
       <div class="post-date text-faded">
         <AppDate :timestamp="post.publishedAt" />
       </div>
-
     </div>
-
   </div>
 </template>
 
 <script>
-
+import PostEditor from '@/components/PostEditor'
 export default {
+  components: { PostEditor },
   props: {
     posts: {
       required: true,
       type: Array
+    }
+  },
+  data () {
+    return {
+      editing: null
     }
   },
   computed: {
@@ -51,10 +60,12 @@ export default {
   methods: {
     userById (userId) {
       return this.$store.getters.user(userId)
+    },
+    toggleEditMode (id) {
+      this.editing = id === this.editing ? null : id
     }
   }
 }
 </script>
-
 <style scoped>
 </style>
